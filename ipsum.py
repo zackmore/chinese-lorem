afb9c52663c5112b7acb6d7f501da374106817eb
zackmore <ratatata@qq.com>
2013-09-05 09:57:14 +0800

    fix last word is comma bug; add -p argument

--- ipsum.py
+++ ipsum.py
@@ -2,6 +2,7 @@
 
 import math
 import random
+import argparse
 
 punc_list = [u'，', u'。', u'！', u'？']
 all_words = open('word.txt').read()
@@ -9,7 +10,7 @@ word_list = all_words.decode('utf-8').split('\n')[:-1]
 
 
 class LoremGenerator(object):
-    def __init__(self, paragraph=random.randint(1, 5)):
+    def __init__(self, paragraph):
         self.paragraph = paragraph
 
     def generate(self):
@@ -20,7 +21,7 @@ class LoremGenerator(object):
             for s in range(sentence_per_paragraph):
                 word_per_sentence = random.randint(1, 20)
                 words = random.sample(word_list, word_per_sentence)
-                words.append(random.choice(punc_list))
+                words.append(random.choice(punc_list[1:]))
                 s = ''.join(words)
                 s_list.append(s)
             p_list.append(''.join(s_list))
@@ -30,5 +31,9 @@ class LoremGenerator(object):
 
 
 if __name__ == '__main__':
-    a = LoremGenerator()
+    parser = argparse.ArgumentParser(description='A python script for generating Chinese lorem ipsum')
+    parser.add_argument('-p', dest='para', default=5, type=int, help='Number of paragraphs')
+    args = parser.parse_args()
+
+    a = LoremGenerator(args.para)
     print(a.generate())
